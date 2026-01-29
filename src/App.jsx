7a1b147de1d1c9d84f8e7a650df04a97226c32de
Mantsha Naaz <mantsha6299@gmail.com>
2026-01-29 12:07:46 +0530

    secand commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import CardPage from "./components/CardPage";
+
+function App() {
+  return (
+    <>
+      <CardPage />
+    </>
+  );
+}
+export default App;
---
+++ src/components/CardPage.jsx
@@ -0,0 +1,11 @@
+function CardPage() {
+  console.log("manan",manan)
+  console.log("sanu",sanu)
+  return <>
+  <div>
+    <h1>hello</h1>
+  </div>
+  </>;
+}
+
+export default CardPage;
