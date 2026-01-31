a66efdcbb8f9a4a5f98ab027baaabf84a2b69481
Mantsha Naaz <mantsha6299@gmail.com>
2026-01-31 13:50:34 +0530

    thir commit

--- src/components/CardPage.jsx
+++ src/components/CardPage.jsx
@@ -1,11 +1,50 @@
-function CardPage() {
-  console.log("manan",manan)
-  console.log("sanu",sanu)
-  return <>
-  <div>
-    <h1>hello</h1>
-  </div>
-  </>;
-}
+import productsData from "./productsData";
 
+function CardPage(){
+  return (
+  <> 
+  {/* <div className="p-6 max-w-3xl mx-auto">
+    <h1 className="text-2xl font-bold mb-4">Simple card</h1> */}
+
+    {/* //products */}
+    <div className=" text-pink-400 grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+     {productsData.map((item) =>{
+      return(
+        <div key={item.id} className="border p-4 rounded shadow">
+        <img 
+        src={item.image}
+        alt={item.name}
+        className="w-full h-48 object-cover rounded"/>
+        <h2 className="font-bold">{item.name}</h2>
+        <p>Price:₹{item.price}</p>
+        <p>Size:₹{item.size}</p>
+      </div>
+      )
+     })}
+    </div>  
+  </>)
+}
 export default CardPage;
+
+
+// import { useState } from "react";
+// function CardPage() {
+//   // console.log("manan",manan)
+//   // console.log("sanu",sanu)
+//   const [count,setCount] = useState(0)
+
+
+//   const handleClickButton = () =>{
+//     // setCount(prev (...prev,count+1))
+//     setCount(count+1)
+//   }
+
+//   return <>
+//   <div>
+//     <h1>hello</h1>
+//     <button className="bg-black border-4 hover:border-rose-900 rounded-full text-white py-0.5 px-96" onClick={handleClickButton}>clickme = {count}</button>
+//   </div>
+//   </>;
+// }
+
+// export default CardPage;
---
+++ src/components/productsData.js
@@ -0,0 +1,41 @@
+
+const productsData = [
+    {
+    id:"1",
+    name : "tshirt",
+    price:"2100",
+    size :"M",
+    image:"images/product-15.jpg",
+    },
+    {
+        id:"2",
+        name:"shirt",
+        price:"2500",
+        size :"M",
+        image:"images/product-16.jpg",
+    },
+    {
+        id:"3",
+        name:"shirt",
+        price:"2500",
+        size :"M",
+        image:"images/product15.jpg",
+    },
+    {
+        id:"4",
+        name:"shirt",
+        price:"2500",
+        size :"M",
+         image:"images/product16.jpg",
+    },
+    {
+        id:"5",
+        name:"shirt",
+        price:"2500",
+        size :"M",
+         image:"images/product17.jpg",
+    },
+]
+export default productsData;
+    
+    
